c22b4db3a7c252f11a0b15995b2da4efff7b1f41
mprreddy51 <44917965+mprreddy51@users.noreply.github.com>
2023-03-16 14:53:42 -0500

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-west-2"
-    access_key = "AKIAWSSTFVR7JN64QXT4"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
 resource "aws_instance" "myec2"{
     ami = "ami-0b029b1931b347543"
